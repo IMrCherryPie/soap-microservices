c4e156779397f194932efb0162ebe76aba08b7d0
Cherry Pie <Mr.CherryPieIn161@gmail.com>
2021-12-20 16:46:42 +0300

    To fill

---
+++ flux-client/src/main/java/com/example/fluxclient/FluxClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxClientApplication.class, args);
+    }
+
+}
---
+++ flux-client/src/test/java/com/example/fluxclient/FluxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
